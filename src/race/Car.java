09ae62b5984eebe1eef444e0a71d1e83e2f3156d
nusubbotin <nu.subbotin@ya.ru>
2022-10-24 17:02:31 +0300

    Enum. Инициализация проекта кодом с предыдущей домашки

---
+++ src/race/Car.java
@@ -0,0 +1,29 @@
+package race;
+
+public class Car extends Transport implements CompetingTransport  {
+
+    public Car(String brand, String model, float engineVolume) {
+        super(brand, model, engineVolume);
+    }
+
+    public void startMoving(){
+        System.out.println("Прогреть двиготель - проехать 3 разогравочных круга");
+        System.out.println("Стартовать по фигналу");
+    };
+
+    public void stopMoving(){
+        System.out.println("пересечь черту ФИНИШ на последнем круге");
+    };
+
+    public void pitStop(){
+        System.out.println("Гонщик останавливается");
+        System.out.println("Команда меняет шины, делает дозаправку");
+        System.out.println("Гонщие выезжает на трек");
+    }
+    public void bestCircleTime(){
+        System.out.println("Запросить информацию у диспетчера гонки");
+    }
+    public void maximumSpeed(){
+        System.out.println("Запросить информацию у диспетчера гонки по максимальной скорости");
+    }
+}
